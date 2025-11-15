7ce0282f902d47aa9b977b2232073139fc9035c2
Curro Portillo <211curroportillo@gmail.com>
2025-11-15 12:59:15 +0100

    ejercicio 57

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int altura = Integer.parseInt(System.console().readLine("Introduzca la altura de la figura: "));
+        int espacios = 0;
+        int base = 0;
+        int fila = altura;
+        
+        while(base < altura){
+            for(int i = 0; i < espacios; i++){
+                System.out.print(" ");
+            }
+            for(int i = 1; i<=fila; i++){
+                if(base == 0)
+                    System.out.print("*");
+                    else if(i == 1 || i == fila)
+                            System.out.print("*");
+                            else
+                                System.out.print(" ");
+            }
+            System.out.println();
+            base++;
+            espacios++;
+            fila--;     
+        }
     }
 }
